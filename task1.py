524d56bfd5497ab8a39ca2681d238215de381908
Simon Zheng <simz97@outlook.com>
2020-02-10 23:41:36 -0500

    Trying to remove rows with missing values

--- task1.py
+++ task1.py
@@ -1,19 +1,15 @@
 from __future__ import print_function
 import numpy as np
-import pandas as pd
-
-def read_and_print_csv(*args):
-    for filename in args:
-        print(f"### {filename} ###")
-        df = pd.read_csv(filename, sep=',', header=None)
-        print(df.values)
+import scipy as sp
 
 def main():
-    # read_and_print_csv("adult.all", "ionosphere.data")
     adult_cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income-exceeds']
-    adult_df = pd.read_csv("adult.all", usecols=adult_cols)[adult_cols]
-    print(adult_df.values)
-    adult_df.to_csv("adult.clean.data")
+    adult_data = np.genfromtxt("adult.all", delimiter=',', dtype='unicode', encoding=None, names=adult_cols, autostrip=True, missing_values='?')
+    print(adult_data[32565])
+    np.logical_and(adult_data == '?')
+    # adult_data = adult_data[~np.any(np.logical_and(np.isnan(adult_data), adult_data == '?'), axis=0)]
+    # print(adult_data[32565])
+    # np.savetxt("adult.clean.data", adult_data, delimiter=", ", fmt="%s")
     return 0
 
 if __name__ == '__main__':
