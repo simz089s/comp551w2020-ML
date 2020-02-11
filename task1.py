58137bce4449580539fe3983812521fc18b39adf
Simon Zheng <simz97@outlook.com>
2020-02-11 02:43:40 -0500

    Clean data script

--- task1.py
+++ task1.py
@@ -1,15 +1,37 @@
 from __future__ import print_function
 import numpy as np
 import scipy as sp
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def clean_data(save_to_csv):
+    """TODO: remove ionosphere headers??"""
+    ionosphere_cols = list(range(35))
+    ionosphere_df = pd.read_csv("ionosphere.data", names=ionosphere_cols, usecols=ionosphere_cols)[ionosphere_cols]
+    ionosphere_df.dropna(how='any', inplace=True)
+    if save_to_csv:
+        ionosphere_df.to_csv("ionosphere.clean.data", index=False, header=False)
+
+    adult_cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income-exceeds']
+    adult_df = pd.read_csv("adult.all", names=adult_cols, usecols=adult_cols, na_values=['?'], skipinitialspace=True)[adult_cols]
+    adult_df.dropna(how='any', inplace=True)
+    if save_to_csv:
+        adult_df.to_csv("adult.clean.data", index=False, header=False)
+
+    return (ionosphere_df, adult_df)
+
+def stats_tests(df_tuple):
+    ionosphere = df_tuple[0]
+    print(ionosphere)
+    adult = df_tuple[1]
+    # plt.scatter('x', 'y', data=ionosphere)
+    # plt.plot(ionosphere)
+    # plt.imshow(ionosphere)
+    ionosphere.plot(kind='scatter', x=3, y=4, color='teal')
+    plt.show()
 
 def main():
-    adult_cols = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income-exceeds']
-    adult_data = np.genfromtxt("adult.all", delimiter=',', dtype='unicode', encoding=None, names=adult_cols, autostrip=True, missing_values='?')
-    print(adult_data[32565])
-    np.logical_and(adult_data == '?')
-    # adult_data = adult_data[~np.any(np.logical_and(np.isnan(adult_data), adult_data == '?'), axis=0)]
-    # print(adult_data[32565])
-    # np.savetxt("adult.clean.data", adult_data, delimiter=", ", fmt="%s")
+    stats_tests(clean_data(False))
     return 0
 
 if __name__ == '__main__':
